1b530f8569bb05dc440d09aa9932dc6a4cae1a95
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2023-03-09 09:32:47 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import {
 
 import routerProvider from "@pankod/refine-react-router-v6";
 import dataProvider from "@pankod/refine-simple-rest";
+import { MuiInferencer } from "@pankod/refine-inferencer/mui";
 
 function App() {
   return (
@@ -29,6 +30,15 @@ function App() {
             Layout={Layout}
             ReadyPage={ReadyPage}
             catchAll={<ErrorComponent />}
+            resources={[
+              {
+                name: 'products',
+                list: MuiInferencer,
+                create: MuiInferencer,
+                edit: MuiInferencer,
+                show: MuiInferencer,
+              },
+            ]}
             routerProvider={routerProvider}
             dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
           />
